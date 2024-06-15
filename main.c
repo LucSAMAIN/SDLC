50a9f390ede26d475190189feedfaab74bcf9fe7
luc.samain <luc.samain2@gmail.com>
2024-06-15 19:15:20 +0200

    damier tuto

--- main.c
+++ main.c
@@ -25,22 +25,65 @@ SDL_Color WHITE = {255, 255, 255, 255};
 ////////////////////////////////////////////////
 
 int setWindowColor(SDL_Renderer *rendeur, SDL_Color color);
+void shakeItUP(SDL_Window *myWindow);
+int initRendererAndWindow(SDL_Window **resultWindow, SDL_Renderer **resultRendeur);
 
 ////////////////////////////////////////////////
 /////////////// DEFINITIONS/////////////////////
 ////////////////////////////////////////////////
+
+int initRendererAndWindow(SDL_Window **resultWindow, SDL_Renderer **resultRendeur)
+{
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "Erreur à SDL_Init: %s", SDL_GetError());
+        return -1;
+    }
+
+    *resultWindow = SDL_CreateWindow(
+        "MaPremiereFenetre", // title
+        SDL_WINDOWPOS_CENTERED, // x
+        SDL_WINDOWPOS_CENTERED, // y
+        640, // witdh
+        480, // height
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE // flags
+    );
+
+    if(*resultWindow == NULL)
+    {
+        fprintf(stderr, "Erreur à SDL_CreateWindow: %s", SDL_GetError());
+        return -1;
+    }
+
+    *resultRendeur = SDL_CreateRenderer(
+        *resultWindow, // fenetre en question
+        -1, // pilot - gere des truc mais aussi pas besoin de mettre le drapeau d'apres...
+        SDL_RENDERER_ACCELERATED // via gpu
+    );
+
+    if(*resultRendeur == NULL)
+    {
+        fprintf(stderr, "Erreur à SDL_CreateRenderer: %s", SDL_GetError());
+        return -1;
+    }
+
+    return 0;
+
+}
 
 int setWindowColor(SDL_Renderer *rendeur, SDL_Color color)
 {
     // on choisit la couleur sur le pinceau:
     if(SDL_SetRenderDrawColor(rendeur, color.r, color.g, color.b, color.a) != 0)
     {
+        fprintf(stderr, "Erreur à SDL_SetRenderDrawColor: %s", SDL_GetError());
         return -1;
     }
 
     // on peint en entier la fenetre associé a notre rendeur
     if(SDL_RenderClear(rendeur) != 0)
     {
+        fprintf(stderr, "Erreur à SDL_RenderClear: %s", SDL_GetError());
         return -1;
     }
 
@@ -89,85 +132,33 @@ int main(int argc, char *argv[])
 {
 
     SDL_Window *window = NULL;
-    SDL_Renderer *rendeur = NULL;
+    SDL_Renderer *renderer = NULL;
     int status = EXIT_SUCCESS;
 
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    if( 0 != initRendererAndWindow(&window, &renderer))
     {
-        fprintf(stderr, "Erreur à SDL_Init: %s", SDL_GetError());
         status = EXIT_FAILURE;
         goto Quit;
     }
 
-    window = SDL_CreateWindow(
-        "MaPremiereFenetre", // title
-        SDL_WINDOWPOS_CENTERED, // x
-        SDL_WINDOWPOS_CENTERED, // y
-        640, // witdh
-        480, // height
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE // flags
-    );
 
-    if(window == NULL)
-    {
-        fprintf(stderr, "Erreur à SDL_CreateWindow: %s", SDL_GetError());
-        status = EXIT_FAILURE;
-        goto Quit;
+    SDL_SetRenderDrawColor(renderer, WHITE.r, WHITE.g, WHITE.b, WHITE.a);
+
+    // damier du tuto:
+
+    SDL_Rect rect[60];
+    size_t i = 0;
+    for(i = 0; i < 50; i++)
+    {   
+        rect[i].w = 50;
+        rect[i].h = 50;
+        rect[i].x = 100 * (i % 5) + 50 * ((i / 5) % 2);
+        rect[i].y = 50 * (i / 5);
     }
+    SDL_RenderFillRects(renderer, rect, 50); 
+    SDL_RenderPresent(renderer);
 
-    // shakeItUP(window);
-
-    rendeur = SDL_CreateRenderer(
-        window, // fenetre en question
-        -1, // pilot - gere des truc mais aussi pas besoin de mettre le drapeau d'apres...
-        SDL_RENDERER_ACCELERATED // via gpu
-    );
-
-    if(rendeur == NULL)
-    {
-        fprintf(stderr, "Erreur à SDL_CreateRenderer: %s", SDL_GetError());
-        status = EXIT_FAILURE;
-        goto Quit;
-    }
-    
-
-    // 
-    // Draw point and line:
-    //
-
-    if(0 != SDL_SetRenderDrawColor(rendeur, WHITE.r, WHITE.g, WHITE.b, WHITE.a))
-    {
-        fprintf(stderr, "Erreur à SDL_SetRenderDrawColor: %s", SDL_GetError());
-        status = EXIT_FAILURE;
-        goto Quit;
-    }
-
-    SDL_RenderDrawLine(
-        rendeur,
-        100, 100,
-        200, 100
-    );
-    SDL_RenderPresent(rendeur);
-    SDL_Delay(500);
-
-    SDL_RenderDrawLine(
-        rendeur,
-        200, 100,
-        200, 300
-    );
-    SDL_RenderPresent(rendeur);
-    SDL_Delay(500);
-
-    SDL_RenderDrawLine(
-        rendeur,
-        200, 300,
-        300, 300
-    );
-    SDL_RenderPresent(rendeur);
-    SDL_Delay(500);
-
-
-
+   
 
     // Attendre pour constater le résultat:
     SDL_Delay(3000);
@@ -178,8 +169,8 @@ int main(int argc, char *argv[])
 Quit:
     if(window != NULL)
         SDL_DestroyWindow(window);
-    if(rendeur != NULL)
-        SDL_DestroyRenderer(rendeur);
+    if(renderer != NULL)
+        SDL_DestroyRenderer(renderer);
 
     SDL_Quit();
     return status;
