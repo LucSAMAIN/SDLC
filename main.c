195c737b535093638c13fc89838b841de0c38d84
luc.samain <luc.samain2@gmail.com>
2024-06-15 14:22:37 +0200

    ma premiere fenetre + delay

--- main.c
+++ main.c
@@ -11,7 +11,26 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    SDL_Window *window = SDL_CreateWindow(
+        "MaPremiereFenetre", // title
+        SDL_WINDOWPOS_CENTERED, // x
+        SDL_WINDOWPOS_CENTERED, // y
+        640, // witdh
+        480, // height
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE // flags
+    );
 
-    SDL_Quit(void);
+    if(window == NULL)
+    {
+        fprintf(stderr, "Erreur à SDL_CreateWindow: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    // pour éviter le thrown away de la fenetre qui
+    // vient d'être créée:
+    SDL_Delay(3000); // pause en ms
+
+
+    SDL_Quit();
     return EXIT_SUCCESS;
 }
