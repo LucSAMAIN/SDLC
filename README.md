bf7917961283dad183e4599b364857dc03dd2900
luc.samain <luc.samain2@gmail.com>
2024-06-15 14:28:37 +0200

    ecraser fenetrer et quit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SDLC
 
+## Requirements:
+Il faut installer les dépendances liés à SDL !
+Allez check: https://zestedesavoir.com/tutoriels/1014/utiliser-la-sdl-en-langage-c/la-sdl/#2-configurer-un-projet-avec-la-sdl
+
+
+
+
+## Que fait ce projet:
 suivi du TUTO suivant: https://zestedesavoir.com/tutoriels/1014/utiliser-la-sdl-en-langage-c/les-premieres-fenetres/
--- main.c
+++ main.c
@@ -5,13 +5,17 @@
 int main(int argc, char *argv[])
 {
 
+    SDL_Window *window = NULL;
+    int status = EXIT_SUCCESS;
+
     if(SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         fprintf(stderr, "Erreur à SDL_Init: %s", SDL_GetError());
-        return EXIT_FAILURE;
+        status = EXIT_FAILURE;
+        goto Quit;
     }
 
-    SDL_Window *window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "MaPremiereFenetre", // title
         SDL_WINDOWPOS_CENTERED, // x
         SDL_WINDOWPOS_CENTERED, // y
@@ -23,7 +27,8 @@ int main(int argc, char *argv[])
     if(window == NULL)
     {
         fprintf(stderr, "Erreur à SDL_CreateWindow: %s", SDL_GetError());
-        return EXIT_FAILURE;
+        status = EXIT_FAILURE;
+        goto Quit;
     }
 
     // pour éviter le thrown away de la fenetre qui
@@ -31,6 +36,9 @@ int main(int argc, char *argv[])
     SDL_Delay(3000); // pause en ms
 
 
+    SDL_DestroyWindow(window);
+
+Quit:
     SDL_Quit();
-    return EXIT_SUCCESS;
+    return status;
 }
